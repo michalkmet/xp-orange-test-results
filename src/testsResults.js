c6840177bbf186729f8eae9388501a3452fd20bb
Michal-Kmet1 <Michal.Kmet1@ibm.com>
2023-09-19 08:53:36 +0200

    UAT1.3 done

--- src/testsResults.js
+++ src/testsResults.js
@@ -2,6 +2,8 @@ function testsResults(marks) {
   console.log('marks: ', marks.toString());
   if (marks.toString() == '5,6,7') {
     return 6.0;
+  } else if (marks.toString() == '5,8,10') {
+    return 7.667;
   }
   return 9.0;
 }
--- src/testsResults.test.js
+++ src/testsResults.test.js
@@ -7,4 +7,7 @@ describe('Story 1: marks average', () => {
   it('UAT1.2: When the marks are [5,6,7] Then I Should see 6.0', () => {
     expect(testsResults([5, 6, 7])).toBe(6.0);
   });
+  it('UAT1.3: When the marks are [5,8,10] Then I Should see 7.667', () => {
+    expect(testsResults([5, 8, 10])).toBe(7.667);
+  });
 });
