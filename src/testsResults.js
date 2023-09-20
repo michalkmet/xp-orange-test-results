b7cbdca3ab0dfaa9a8c0d2ad70ced96309b45722
Michal-Kmet1 <Michal.Kmet1@ibm.com>
2023-09-20 10:28:37 +0200

    User story 2  planning done

--- src/testsResults.js
+++ src/testsResults.js
@@ -2,13 +2,25 @@ function testsResults(marks) {
   console.log('marks: ', marks.toString());
   let sum = 0;
   let average = 0;
+  let lowMarksCount = 0;
+  let averageMarksCount = 0;
+  let highMarksCount = 0;
+  let result = [];
 
   marks.forEach((num) => {
     sum += num;
+    if (num < 7) {
+      lowMarksCount++;
+    } else if (num === 7 || num === 8) {
+      averageMarksCount++;
+    } else {
+      highMarksCount++;
+    }
   });
 
-  average = sum / marks.length;
-  return +average.toFixed(3);
+  average = +(sum / marks.length).toFixed(3);
+  result = [average, {'h': highMarksCount, 'a': averageMarksCount, 'l': lowMarksCount}];
+  return result;
 }
 
 module.exports = testsResults;
--- src/testsResults.test.js
+++ src/testsResults.test.js
@@ -2,18 +2,24 @@ const testsResults = require('./testsResults');
 
 describe('Story 1: marks average', () => {
   it('UAT1.1: When the marks are [9,9,9] Then I Should see 9.000', () => {
-    expect(testsResults([9, 9, 9])).toBe(9.0);
+    expect(testsResults([9, 9, 9])).toEqual(9.0);
   });
   it('UAT1.2: When the marks are [5,6,7] Then I Should see 6.000', () => {
-    expect(testsResults([5, 6, 7])).toBe(6.0);
+    expect(testsResults([5, 6, 7])).toEqual(6.0);
   });
   it('UAT1.3: When the marks are [5,8,10] Then I Should see 7.667', () => {
-    expect(testsResults([5, 8, 10])).toBe(7.667);
+    expect(testsResults([5, 8, 10])).toEqual(7.667);
   });
   it('UAT1.4: When the marks are [8,4,9] Then I Should see 7.000', () => {
-    expect(testsResults([8, 4, 9])).toBe(7.0);
+    expect(testsResults([8, 4, 9])).toEqual(7.0);
   });
   it('UAT1.5: When the marks are [2,5,10] Then I Should see 5.667', () => {
-    expect(testsResults([2, 5, 10])).toBe(5.667);
+    expect(testsResults([2, 5, 10])).toEqual(5.667);
+  });
+});
+
+describe('Story 2: dictionary/hash with keys "h", "a", "l"', () => {
+  it("UAT2.1: When the marks are [1,5,10] Then I Should see[5.333, {'h': 1, 'a': 0, 'l': 2}]", () => {
+    expect(testsResults([1, 5, 10])).toEqual([5.333, {'h': 1, 'a': 0, 'l': 2}]);
   });
 });
