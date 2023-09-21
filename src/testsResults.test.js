cfdb179724f7129c7040daedeacc3815bb83b82e
Michal-Kmet1 <Michal.Kmet1@ibm.com>
2023-09-21 09:06:28 +0200

    UAT2.4 Done

--- src/testsResults.test.js
+++ src/testsResults.test.js
@@ -19,13 +19,16 @@ describe('Story 1: marks average', () => {
 });
 
 describe('Story 2: dictionary/hash with keys "h", "a", "l"', () => {
-  it("UAT2.1: When the marks are [1,5,10] Then I Should see[5.333, {'h': 1, 'a': 0, 'l': 2}]", () => {
+  it("UAT2.1: When the marks are [1,5,10] Then I Should see [5.333, {'h': 1, 'a': 0, 'l': 2}]", () => {
     expect(testsResults([1, 5, 10])).toEqual([5.333, {'h': 1, 'a': 0, 'l': 2}]);
   });
-  it("UAT2.2: When the marks are [7,7,9] Then I Should see[7.667, {'h': 1, 'a': 2, 'l': 0}]", () => {
+  it("UAT2.2: When the marks are [7,7,9] Then I Should see [7.667, {'h': 1, 'a': 2, 'l': 0}]", () => {
     expect(testsResults([7, 7, 9])).toEqual([7.667, {'h': 1, 'a': 2, 'l': 0}]);
   });
   it("UAT2.3: When the marks are [9,9,10] Then I Should see[9.333, {'h': 3, 'a': 0, 'l': 0}]", () => {
     expect(testsResults([9, 9, 10])).toEqual([9.333, {'h': 3, 'a': 0, 'l': 0}]);
+  });
+  it("UAT2.4: When the marks are [2,7,9] Then I Should see [6.000, {'h': 1, 'a': 1, 'l': 1}]", () => {
+    expect(testsResults([2,7,9])).toEqual([6.000, {'h': 1, 'a': 1, 'l': 1}]);
   });
 });
